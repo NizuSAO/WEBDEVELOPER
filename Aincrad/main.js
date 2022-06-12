8d8df0cf235e30c607bc8c971a70de56bc795a69
NizuSAO <nizusao@gmail.com>
2022-06-12 19:27:40 -0300

    icecream

---
+++ Aincrad/main.js
@@ -0,0 +1,18 @@
+
+var nombre = "Nizusao";
+var altutra = 167;
+var edad = 18;
+
+function suma(a, b)
+{
+    let resul = a + b;
+    alert("les damos la bienvenida a "+ resul);
+}
+
+function mostrarNombre(n)
+{
+    document.getElementById("mensaje").innerHTML = n;
+}
+suma("Aincrad","Link start");
+
+mostrarNombre("Nizusao");
